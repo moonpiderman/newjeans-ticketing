2a10bae90d53a4ad8fb411de80a022a2116a8519
moonpiderman <lotsofchuck@gmail.com>
2024-04-21 21:01:15 +0900

    Feat: 대기열 스케쥴링

--- src/main/java/newjeans/tickets/ticketserver/TicketServerApplication.java
+++ src/main/java/newjeans/tickets/ticketserver/TicketServerApplication.java
@@ -2,8 +2,10 @@ package newjeans.tickets.ticketserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class TicketServerApplication {
 
     public static void main(String[] args) {
--- src/main/java/newjeans/tickets/ticketserver/service/UserQueueService.java
+++ src/main/java/newjeans/tickets/ticketserver/service/UserQueueService.java
@@ -1,22 +1,31 @@
 package newjeans.tickets.ticketserver.service;
 
 import lombok.RequiredArgsConstructor;
-import newjeans.tickets.ticketserver.dto.AllowUserResponse;
-import newjeans.tickets.ticketserver.exception.ApplicationException;
+import lombok.extern.slf4j.Slf4j;
 import newjeans.tickets.ticketserver.exception.ErrorCode;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.connection.zset.Tuple;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.data.redis.core.ScanOptions;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
+import reactor.util.function.Tuples;
 
 import java.time.Instant;
 import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class UserQueueService {
     private final ReactiveRedisTemplate<String, String> reactiveRedisTemplate;
     private final String USER_WAIT_KEY = "users:queue:%s:wait";
+    private final String USER_WAIT_KEY_FOR_SCAN = "users:queue:*:wait";
     private final String USER_PROCEED_KEY = "users:queue:%s:proceed";
+
+    @Value("${scheduler.enabled}")
+    private Boolean scheduling = false;
 
     public Mono<Long> registerWaitQueue(final String queue, final Long userId) {
         var unixTimeStamp = Instant.now().getEpochSecond();
@@ -51,5 +60,26 @@ public class UserQueueService {
         return reactiveRedisTemplate.opsForZSet().rank(USER_WAIT_KEY.formatted(queue), userId.toString())
                 .defaultIfEmpty(-1L)
                 .map(rank -> rank >= 0 ? rank + 1: rank);
+    }
+
+    @Scheduled(initialDelay = 5000, fixedRate = 3000)
+    public void scheduleAllowUser() {
+        if (!scheduling) {
+            log.info("PASSED SCHEUDLING");
+            return;
+        }
+
+        log.info("called scheduling ...! ");
+
+        var maxAllowUserCount = 3L;
+
+        reactiveRedisTemplate.scan(ScanOptions.scanOptions()
+                .match(USER_WAIT_KEY_FOR_SCAN)
+                .count(100)
+                .build())
+                .map(key -> key.split(":")[2])
+                .flatMap(queue -> allowUser(queue, maxAllowUserCount).map(allowed -> Tuples.of(queue, allowed)))
+                .doOnNext(tuple -> log.info("Tried %d and allowed %s members of %s queue.".formatted(maxAllowUserCount, tuple.getT1(), tuple.getT2())))
+                .subscribe();
     }
 }
