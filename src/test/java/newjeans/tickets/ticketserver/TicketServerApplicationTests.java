3b509c68b9f2bc236c0a1e2a7751652cca7b9f21
moonpiderman <lotsofchuck@gmail.com>
2024-04-21 15:44:36 +0900

    newjeans ticket init

---
+++ src/test/java/newjeans/tickets/ticketserver/TicketServerApplicationTests.java
@@ -0,0 +1,13 @@
+package newjeans.tickets.ticketserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
